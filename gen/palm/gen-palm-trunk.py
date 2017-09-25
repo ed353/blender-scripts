e8cea3086ea5cda84db2c2d32842650b1b481155
eadonah <eadonah@sandia.gov>
2017-09-24 20:58:28 -0700

    finally figured out how to get top face center median in world space :)

--- gen/palm/gen-palm-trunk.py
+++ gen/palm/gen-palm-trunk.py
@@ -10,117 +10,55 @@ import math
 import random
 
 # custom module imports
-import os, sys
-
-file_path = os.getcwd() # if started from icon, cwd = ~
-scripts_path = os.path.join(file_path, 'blender-scripts')
-utils_path = os.path.join(scripts_path, 'utils')
-
-if utils_path not in sys.path:
-    sys.path.append(utils_path)
-
 from blendutils import *
 
-scene = bpy.context.scene
-
-# def start_trunk(scale=1.0):
+'''
+Make a "chunk" of the trunk with its base at a specified location.
+'''
 def make_chunk_at(bottom_origin, scale=1.0):
     
     z_trans = mathutils.Vector((0., 0., 1.))
     location = bottom_origin + z_trans
+    
     bpy.ops.mesh.primitive_cylinder_add(
         radius=1,
         vertices=10,
         location=location
-        # enter_editmode=True
     )
+
+    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
     
     chunk = bpy.context.object
     select_none()
     
     return chunk
 
-def get_top_center(chunk_obj):
+def get_top_center(obj):
     
-    select_none()
-    select_obj_by_name(chunk_obj.name)
-    
-    bpy.ops.object.mode_set(mode='EDIT')
-    top_idx = get_top_face_idx()
-    
-    print(top_idx)
-    obj = bpy.context.edit_object
-    bm = bmesh.from_edit_mesh(obj.data)
-    top_center = bm.faces[top_idx].calc_center_median()
-    
-    bpy.ops.object.mode_set(mode='OBJECT')
-    
-    return top_center
+  bm = bmesh.new()
+  bm.from_mesh(obj.data)
 
-def get_top_face_idx():
-    
-    obj = bpy.context.edit_object
-    msh = obj.data
-    bm = bmesh.from_edit_mesh(msh)
-    bm.normal_update()
-    
-    idx = -1
-    z_vec = mathutils.Vector((0.0, 0.0, 1.0))
-    bm.faces.ensure_lookup_table()
-    for i in range(len(bm.faces)):
-        n = bm.faces[i].normal
-        if(n.dot(z_vec) > 0.5):
-            idx = i
-            break
-        
-    return idx
+  top_idx = get_top_face_idx(bm)
+  top_ctr = bm.faces[top_idx].calc_center_median()
+  top_ctr = obj.matrix_world * top_ctr
 
-def select_obj_by_name(obj_name):
-    obj = bpy.data.objects[obj_name]
-    obj.select = True
+  return top_ctr
+
+def get_top_face_idx(bm):
     
-def extrude_trunk(dist):
-    
-    r_angle = random.random() * 360.0
-    
-    vec_x = mathutils.Vector((1., 0., 0.))
-    mat_rot = mathutils.Matrix.Rotation(
-        math.radians(r_angle), 
-        4, 'Z')
-    rot_axis = mat_rot * vec_x
-    
-    # tilt face a bit
-    bpy.ops.transform.rotate(
-        value = -0.15,
-        axis = rot_axis
-    )
-    
-    # shrink
-    bpy.ops.transform.resize(
-        value = (0.9, 0.9, 0.9)
-    )
-    # extrude
-    obj = bpy.context.edit_object
-    msh = obj.data
-    bm = bmesh.from_edit_mesh(msh)
-    bm.normal_update()
-    
-    top_idx = get_top_face_idx()
-    trans_vec = bm.faces[top_idx].normal * dist
-    
-    bpy.ops.mesh.extrude_region_move(
-        TRANSFORM_OT_translate={
-            "value": trans_vec
-            }
-        )
-        
-    bm.normal_update()
-    
-    # tilt face a bit
-    bpy.ops.transform.rotate(
-        value = 0.15,
-        axis = rot_axis
-    )
+  bm.faces.ensure_lookup_table()
+  bm.normal_update()
+  
+  idx = -1
+  z_vec = mathutils.Vector((0.0, 0.0, 1.0))
+  
+  for i in range(len(bm.faces)):
+    n = bm.faces[i].normal
+    if(n.dot(z_vec) > 0.5):
+      idx = i
+      break
+      
+  return idx
 
 if __name__=='__main__':
     
@@ -128,16 +66,13 @@ if __name__=='__main__':
     
     prev_top = mathutils.Vector((0.0, 0.0, 0.0))
 
-    for i in range(1):
-        chunk = make_chunk_at(prev_top)
-        print(chunk.name)
-        
-        prev_top = get_top_center(chunk)
-        top_idx = get_top_idx()
-        print(top_idx)
-        # select_none()
-        
-        print(prev_top)
+    for i in range(5):
+      
+      # make a chunk of le palm tree
+      chunk = make_chunk_at(prev_top)
+      # TODO: squeeze and tilt the trunk a lil
+      prev_top = get_top_center(chunk)
+      
    
     # object_mode()
     # select_none()
